33d97a944b02001fcdb142081322e19a944730fb
tnmas <tsigereda.nebai@ost.ch>
2023-04-10 15:54:06 +0000

    Test vms

--- controllers/labinstance_controller_test.go
+++ controllers/labinstance_controller_test.go
@@ -8,12 +8,18 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
+	kubevirtv1 "kubevirt.io/api/core/v1"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
+
+// TODO: Test the status of the pods and VMs
 
 var _ = Describe("LabInstance Controller", func() {
 
@@ -24,16 +30,25 @@ var _ = Describe("LabInstance Controller", func() {
 		LabInstanceNamespace = "test-namespace"
 		Timeout              = time.Second * 10
 		Interval             = time.Millisecond * 250
+		config               = "#cloud-config\npassword: ubuntu\nchpasswd:\n{ expire: False }\nssh_authorized_keys:\n- <your-ssh-pub-key>\npackages:\n- qemu-guest-agent\nruncmd\n- [ systemctl, start, qemu-guest-agent ]"
 	)
 	var (
 		testLabInstance *ltbv1alpha1.LabInstance
 		testLabTemplate *ltbv1alpha1.LabTemplate
 		testPod         *corev1.Pod
+		testVM          *kubevirtv1.VirtualMachine
 		fakeClient      client.Client
 		err             error
+		running         bool
 	)
 
+	schemeBuilder := runtime.NewSchemeBuilder(kubevirtv1.AddToScheme)
+	vmScheme := runtime.NewScheme()
+	err = schemeBuilder.AddToScheme(vmScheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	BeforeEach(func() {
+
 		testLabInstance = &ltbv1alpha1.LabInstance{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      LabInstanceName,
@@ -58,10 +73,14 @@ var _ = Describe("LabInstance Controller", func() {
 							Version: "20.04",
 						},
 					},
-				},
-				Connections: []ltbv1alpha1.Connection{
 					{
-						Neighbors: "Test-Node1:1,Test-Node2:1",
+						Name: "test-node-2",
+						Image: ltbv1alpha1.NodeImage{
+							Type:    "ubuntu",
+							Version: "20.04",
+							Kind:    "vm",
+						},
+						Config: config,
 					},
 				},
 			},
@@ -78,6 +97,50 @@ var _ = Describe("LabInstance Controller", func() {
 						Name:    testLabTemplate.Spec.Nodes[0].Name,
 						Image:   testLabTemplate.Spec.Nodes[0].Image.Type + ":" + testLabTemplate.Spec.Nodes[0].Image.Version,
 						Command: []string{"/bin/sleep", "3600"},
+					},
+				},
+			},
+		}
+
+		running = true
+		resources := kubevirtv1.ResourceRequirements{
+			Requests: corev1.ResourceList{"memory": resource.MustParse("1024M")},
+		}
+		cpu := &kubevirtv1.CPU{Cores: 1}
+
+		disk := []kubevirtv1.Disk{
+			{Name: "containerdisk",
+				DiskDevice: kubevirtv1.DiskDevice{
+					Disk: &kubevirtv1.DiskTarget{Bus: "virtio"}}},
+			{Name: "cloudinitdisk", DiskDevice: kubevirtv1.DiskDevice{
+				Disk: &kubevirtv1.DiskTarget{Bus: "virtio"}}},
+		}
+
+		volumes := []kubevirtv1.Volume{
+			{Name: "containerdisk",
+				VolumeSource: kubevirtv1.VolumeSource{
+					ContainerDisk: &kubevirtv1.ContainerDiskSource{Image: "quay.io/containerdisks/" + testLabTemplate.Spec.Nodes[1].Image.Type + ":" + testLabTemplate.Spec.Nodes[1].Image.Version}}},
+			{Name: "cloudinitdisk", VolumeSource: kubevirtv1.VolumeSource{
+				CloudInitNoCloud: &kubevirtv1.CloudInitNoCloudSource{UserData: testLabTemplate.Spec.Nodes[1].Config}}},
+		}
+
+		testVM = &kubevirtv1.VirtualMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      testLabTemplate.Spec.Nodes[1].Name,
+				Namespace: testLabInstance.Namespace,
+			},
+			Spec: kubevirtv1.VirtualMachineSpec{
+				Running: &running,
+				Template: &kubevirtv1.VirtualMachineInstanceTemplateSpec{
+					Spec: kubevirtv1.VirtualMachineInstanceSpec{
+						Domain: kubevirtv1.DomainSpec{
+							Resources: resources,
+							CPU:       cpu,
+							Devices: kubevirtv1.Devices{
+								Disks: disk,
+							},
+						},
+						Volumes: volumes,
 					},
 				},
 			},
@@ -110,6 +173,36 @@ var _ = Describe("LabInstance Controller", func() {
 		It("Should be deleted after deletion", func() {
 			By("Expecting the Pod to be deleted")
 			err = fakeClient.Delete(context.Background(), testPod)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(Succeed())
+		})
+	})
+
+	Context("VirtualMachine within a LabInstance", func() {
+		It("Should not exist before creation", func() {
+			fakeClient = fake.NewClientBuilder().WithScheme(vmScheme).Build()
+			err = fakeClient.Get(context.Background(), types.NamespacedName{Name: testVM.Name, Namespace: testLabInstance.Namespace}, testVM)
+			By("Expecting an error when trying to get the VirtualMachine which does not exist")
+			Expect(err).To(HaveOccurred())
+			Expect(client.IgnoreNotFound(err)).To(Succeed())
+		})
+
+		It("Should exist after creation", func() {
+			By("Expecting the VirtualMachine to be created")
+			err = fakeClient.Create(context.Background(), testVM)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(Succeed())
+		})
+
+		It("Should have a status after creation", func() {
+			By("Expecting the VirtualMachine to have a status")
+			testVMStatus := testVM.Status.Ready
+			Expect(testVMStatus).NotTo(BeNil())
+		})
+
+		It("Should be deleted after deletion", func() {
+			By("Expecting the VirtualMachine to be deleted")
+			err = fakeClient.Delete(context.Background(), testVM)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(err).To(Succeed())
 		})
