c12b81d763df075c5d1dca4acc85a00f21c413ca
tnmas <tsigereda.nebai@ost.ch>
2023-06-15 08:24:47 +0000

    Remove comments

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023 Jan Untersander, Tsigereda Nebai Kidane.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
@@ -32,15 +16,6 @@ import (
 	//+kubebuilder:scaffold:imports
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
-//var cfg *rest.Config
-
-//var K8sClient ctrl.Manager
-
-// var testEnv *envtest.Environment
-
 func TestControllers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Controller Suite")
@@ -49,19 +24,7 @@ func TestControllers(t *testing.T) {
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-	// By("bootstrapping test environment")
-	// testEnv = &envtest.Environment{
-	// 	CRDDirectoryPaths:     []string{filepath.Join("../..", "config", "crd", "bases")},
-	// 	ErrorIfCRDPathMissing: true,
-	// }
-
 	var err error
-	// cfg is defined in this file globally.
-	//cfg, err = testEnv.Start()
-	//Expect(err).NotTo(HaveOccurred())
-	//Expect(cfg).NotTo(BeNil())
-	//cfg = config.GetConfigOrDie()
-	//Expect(cfg).NotTo(BeNil())
 
 	err = scheme.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
@@ -76,13 +39,5 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 
 	initialize()
-	//K8sClient, err = ctrl.NewManager(cfg, ctrl.Options{Scheme: scheme.Scheme})
-	//Expect(err).NotTo(HaveOccurred())
 
 })
-
-// var _ = AfterSuite(func() {
-// 	By("tearing down the test environment")
-// 	err := testEnv.Stop()
-// 	Expect(err).NotTo(HaveOccurred())
-// })
